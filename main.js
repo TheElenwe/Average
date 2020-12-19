dea953f96e67c5b0ff8184c9c384c3a7f8a0f36a
unknown <you@example.com>
2020-12-19 20:08:01 +0200

    Fist commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let Yargs = require('yargs')
+ 
+let Geometry=7 ;
+let Algebra=8;
+let Physics=7;
+
+let average_score =(Geometry +Algebra + Physics )  / 3 ;
+console.log("Τhe average of Geometry,Algebra, Physics is :" + average_score);
