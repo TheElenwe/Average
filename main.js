62ec5ea18c5c2edf6f5a3942a77709cd2f00f000
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2020-12-23 22:42:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let prompt = require('prompt-sync')({sigint: true});
- let num1 = prompt("Enter a number")
- let num2 = prompt("enter number two")
- let num3 = prompt("enter the third number")
+ let num1 = prompt("PLease enter the first number :")
+ let num2 = prompt("Please enter the second number :")
+ let num3 = prompt("Please enter the third number :")
 
 num1 = parseFloat(num1);
 num2 = parseFloat(num2);
@@ -10,4 +10,5 @@ num3 = parseFloat(num3);
 let average_score= (  num1 + num2 + num3 ) / 3 ;
 console.log("Τhe average of Geometry,Algebra, Physics is :" + average_score );
 
+ 
  
